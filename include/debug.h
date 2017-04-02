9ac53ba955fb540af39dd766a7449026634907bf
GeekSlag <kg@sina.com>
2017-04-02 15:26:43 +0800

    MACRO ##__VA_ARGS__

--- include/debug.h
+++ include/debug.h
@@ -1,34 +1,21 @@
-#ifndef _DBG_H_
-#define _DBG_H_
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
 
 #define D_INFO (1)
 #define D_WARNING (2)
 #define D_ERROR (3)
  
-#define PRT_INFO(fmt, ...) printf("\e[1;32m|INFO|\e[0m "fmt, __VA_ARGS__)
-#define PRT_WARNING(fmt, ...) printf("\e[1;33m|WARNING|\e[0m "fmt, __VA_ARGS__)
-#define PRT_ERROR(fmt, ...) printf("\e[1;31m|ERROR|\e[0m "fmt, __VA_ARGS__)
+#define PRT_INFO(fmt, ...) printf("\e[1;32m|INFO|\e[0m "fmt, ##__VA_ARGS__)
+#define PRT_WARNING(fmt, ...) printf("\e[1;33m|WARNING|\e[0m "fmt, ##__VA_ARGS__)
+#define PRT_ERROR(fmt, ...) printf("\e[1;31m|ERROR|\e[0m "fmt, ##__VA_ARGS__)
 #define LOGGER(mod_id, dbg_flg, fmt, ...) \
-    do { \
-        if (D_INFO == dbg_flg) \
-            PRT_INFO(fmt, __VA_ARGS__); \
-        if (D_WARNING == dbg_flg) \
-            PRT_WARNING(fmt, __VA_ARGS__); \
-        if (D_ERROR == dbg_flg) \
-            PRT_ERROR(fmt, __VA_ARGS__); \
-    } while(0) 
-
-/****************************************************************************/
-void prt_args(char *fmt, ...);
-#define PRT_VAR(FORMAT, VAR) prt_args("\e[33m|INFO|\e[0m %-16s : "FORMAT, #VAR, VAR)
-#define DBG_PRT(...)\
-do {\
-	prt_args("[%s %s:%d] ", __FILE__, __FUNCTION__, __LINE__);\
-	prt_args(__VA_ARGS__);\
-} while(0)
-
-void prt_asterisk(void);
-#define PRT_AST() prt_asterisk()
-/****************************************************************************/
+do { \
+    if (D_INFO == dbg_flg) \
+        PRT_INFO(fmt, ##__VA_ARGS__); \
+    if (D_WARNING == dbg_flg) \
+        PRT_WARNING(fmt, ##__VA_ARGS__); \
+    if (D_ERROR == dbg_flg) \
+        PRT_ERROR(fmt, ##__VA_ARGS__); \
+} while(0) 
 
 #endif
--- source/main.c
+++ source/main.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 	unsigned int ret = 0;
 
     LOGGER(0, D_INFO, "ret(%#x)\n", ret);
-    LOGGER(0, D_WARNING, "warning\n", ret);
-    LOGGER(0, D_ERROR, "error!\n", ret);
+    LOGGER(0, D_WARNING, "warning\n");
+    LOGGER(0, D_ERROR, "error!\n");
 
 	return ret;
 }
